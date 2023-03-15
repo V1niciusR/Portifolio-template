3616ea12c13a7712de775c37c210316e111efb18
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-03-15 14:35:48 -0300

    ...

--- JS/Tema.js
+++ JS/Tema.js
@@ -4,3 +4,15 @@ const body = document.body;
 toggleButton.addEventListener("click", function () {
   body.classList.toggle("light-mode");
 });
+
+
+
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', function() {
+  const icon = themeToggle.querySelector('ion-icon');
+  if (icon.getAttribute('name') === 'sunny-outline') {
+    icon.setAttribute('name', 'moon-outline');
+  } else {
+    icon.setAttribute('name', 'sunny-outline');
+  }
+});
